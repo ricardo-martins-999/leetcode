5b3122af109bbfbdb1a654879b7a54be2791ecbe
Ricardo Martins <ricardo.martins.6373@gmail.com>
2026-03-15 14:25:00 -0300

    Solutions for some LeetCode problems

---
+++ easy/0412-fizz-buzz.js
@@ -0,0 +1,34 @@
+/**
+ * Problem: Fizz Buzz
+ * Link: https://leetcode.com/problems/fizz-buzz/
+ * Difficulty: Easy
+ *
+ * Approach:
+ * 1. Iteration + Modulo
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+const fizzBuzz = function(n) {
+  const ans = [];
+
+  for (let i = 1; i <= n; i++) {
+    let str = "";
+
+    if (i % 3 === 0) str += "Fizz";
+    if (i % 5 === 0) str += "Buzz";
+
+    ans.push(str || String(i));
+  }
+
+  return ans;
+}
+
+console.log(fizzBuzz(3))
+console.log(fizzBuzz(5))
+console.log(fizzBuzz(15))
---
+++ easy/3090-maximum-length-substring-with-two-occurrences.js
@@ -0,0 +1,29 @@
+/**
+ * Problem: Maximum Length Substring
+ * Link: https://leetcode.com/problems/maximum-length-substring-with-two-occurrences/
+ * Difficulty: Easy
+ *
+ * Approaches:
+ * 1. Sliding Window
+ */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const maximumLengthSubstringWithTwoOccurrences = function(s) {
+  let left = 0;
+  let max = 0;
+  const map = new Map();
+  for (let right = 0; right < s.length; right++) {
+    map.set(s[right], (map.get(s[right]) || 0) + 1);
+    while (map.get(s[right]) > 2) {
+      map.set(s[left], map.get(s[left]) - 1);
+      left++;
+    }
+    max = Math.max(max, right - left + 1);
+  }
+  return max;
+};
+
+console.log(maximumLengthSubstring("bcbbbcba"));
