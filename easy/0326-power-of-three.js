35c51c061c13352bbba82c4f60fcb04f04d19e51
Ricardo Martins <ricardo.martins.6373@gmail.com>
2026-03-15 18:49:57 -0300

    Solutions for some LeetCode problems

---
+++ easy/0326-power-of-three.js
@@ -0,0 +1,42 @@
+/**
+ * Problem: Power of Three
+ * Link: https://leetcode.com/problems/power-of-three/
+ * Difficulty: Easy
+ *
+ * Approach:
+ * 1. Iterative
+ * 2. Recursive
+ */
+
+/*
+ * 1. Iterative
+ *
+ * Time Complexity: O(log n)
+ * Space Complexity: O(1)
+ */
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+const isPowerOfThreeIterative = function(n) {
+  while (n > 1) {
+    if (n % 3 !== 0) return false;
+    n = n / 3;
+  }
+  return n === 1;
+};
+
+/*
+ * 2. Recursive
+ *
+ * Time Complexity: O(log n)
+ * Space Complexity: O(log n)
+ */
+
+const isPowerOfThreeRecursive = function(n) {
+  if (n === 1) return true;
+  if (n <= 0 || n % 3 !== 0) return false;
+
+  return isPowerOfThreeRecursive(n / 3);
+};
---
+++ easy/0342-power-of-four.js
@@ -0,0 +1,42 @@
+/**
+ * Problem: Power of Four
+ * Link: https://leetcode.com/problems/power-of-four/
+ * Difficulty: Easy
+ *
+ * Approach:
+ * 1. Iterative
+ * 2. Recursive
+ */
+
+/*
+ * 1. Iterative
+ *
+ * Time Complexity: O(log n)
+ * Space Complexity: O(1)
+ */
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+const isPowerOfFourIterative = function(n) {
+  while (n > 1) {
+    if (n % 4 !== 0) return false;
+    n = n / 4;
+  }
+  return n === 1;
+};
+
+/*
+ * 2. Recursive
+ *
+ * Time Complexity: O(log n)
+ * Space Complexity: O(log n)
+ */
+
+const isPowerOfFourRecursive = function(n) {
+  if (n === 1) return true;
+  if (n <= 0 || n % 4 !== 0) return false;
+
+  return isPowerOfFourRecursive(n / 4);
+};
